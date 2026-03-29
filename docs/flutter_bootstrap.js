e645f3d6c36cb370799c8d66a6696e013c84b04d
Asmat Kaur Taunque <asmat@telaeris.com>
2026-03-29 10:01:30 -0500

    Update intro text timing, glow styling, and connections fix

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3803085557" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4111467706" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
